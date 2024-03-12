c7cf6871eda8ba6b36c9ae3ee88fb7ffe7c1096c
Gonzalo Lobos <loboscristiang@gmail.com>
2024-03-12 10:22:04 -0300

    Add grid

--- scripts.js
+++ scripts.js
@@ -1,6 +1,12 @@
 const container = document.querySelector('.container');
+const divs = [];
+let dimensions = 16;
 
-const div = document.createElement('div');
-div.setAttribute("style", `width: ${960 / 16}px; height: ${960 / 16}px`);
-div.classList.add('square');
-container.appendChild(div);
+for(let i = 0; i < dimensions * dimensions; i++){
+  const div = document.createElement('div');
+  div.setAttribute("style", `width: ${960 / dimensions}px; height: ${960 / dimensions}px`);
+  div.classList.add('square');
+  divs.push(div);
+}
+
+divs.forEach((div) => container.appendChild(div));
