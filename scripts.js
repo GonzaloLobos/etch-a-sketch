bc7b776bca8c024e82ff8974cf053736b26a676a
Gonzalo Lobos <loboscristiang@gmail.com>
2024-03-12 10:26:12 -0300

    Add painting functionality

--- scripts.js
+++ scripts.js
@@ -5,8 +5,13 @@ let dimensions = 16;
 for(let i = 0; i < dimensions * dimensions; i++){
   const div = document.createElement('div');
   div.setAttribute("style", `width: ${960 / dimensions}px; height: ${960 / dimensions}px`);
-  div.classList.add('square');
+  div.classList.add('cell');
   divs.push(div);
 }
 
-divs.forEach((div) => container.appendChild(div));
+divs.forEach((div) => {
+  div.addEventListener('mouseenter', function(){
+    div.classList.add('painted');
+  })
+  container.appendChild(div);
+});
