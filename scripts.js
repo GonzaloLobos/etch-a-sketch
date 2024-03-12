d809e364f7119b54aec3bab7b91b63b9d532042e
Gonzalo Lobos <loboscristiang@gmail.com>
2024-03-12 10:54:49 -0300

    Refactor grid generation
    Move grid generation logic into a function
    in order to reuse it.

--- scripts.js
+++ scripts.js
@@ -1,17 +1,27 @@
 const container = document.querySelector('.container');
+const customiseBtn = document.querySelector('.customise');
 const divs = [];
 let dimensions = 16;
 
-for(let i = 0; i < dimensions * dimensions; i++){
-  const div = document.createElement('div');
-  div.setAttribute("style", `width: ${960 / dimensions}px; height: ${960 / dimensions}px`);
-  div.classList.add('cell');
-  divs.push(div);
-}
-
-divs.forEach((div) => {
-  div.addEventListener('mouseenter', function(){
-    div.classList.add('painted');
-  })
-  container.appendChild(div);
+customiseBtn.addEventListener('click', function(){
+  dimensions = parseInt(prompt("Enter a number from 1-100", 16));
 });
+
+function generateGrid(cellsPerSide){
+  divs.forEach((div) => container.removeChild(div));
+  divs.length = 0;
+
+  for(let i = 0; i < cellsPerSide * cellsPerSide; i++){
+    const div = document.createElement('div');
+    div.setAttribute("style", `width: ${960 / cellsPerSide}px; height: ${960 / cellsPerSide}px`);
+    div.classList.add('cell');
+    divs.push(div);
+  }
+
+  divs.forEach((div) => {
+    div.addEventListener('mouseenter', function(){
+      div.classList.add('painted');
+    })
+    container.appendChild(div);
+  });
+}
